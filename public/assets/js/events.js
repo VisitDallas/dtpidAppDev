6e22bd992cb71af4076a92dd225906a4724eca19
Nick Askam <asknickfilms@gmail.com>
2019-12-19 11:32:15 -0600

    added back in variables

--- public/assets/js/events.js
+++ public/assets/js/events.js
@@ -1,6 +1,7 @@
 function doTotalBudgetCalculations(){
-	let totalEventBudgetDisplay = Number(document.getElementById("00N0b00000CbKPj").value);
-	let percentDTPIDFundsTotal = Math.floor(requestedAmount / (totalEventBudget / 100));
+	let totalEventBudgetDisplay = document.getElementById("00N0b00000CbKPj").value;
+	let totalEventBudget = Number(totalEventBudgetDisplay) / 100;
+	let percentDTPIDFundsTotal = Math.floor(requestedAmount / totalEventBudget);
 	document.getElementById("percentDTPIDFundsTotalDisplay").textContent = percentDTPIDFundsTotal;
 	numberWithCommas(totalEventBudgetDisplay.value);
 }
