c5a53333a0a6ac26d6560c87a3b17853b9908f7e
Nick Askam <asknickfilms@gmail.com>
2019-12-19 12:06:41 -0600

    testing number with commas 3

--- public/assets/js/events.js
+++ public/assets/js/events.js
@@ -9,11 +9,11 @@ function totalFundingFunctions() {
 }
 
 function doTotalBudgetCalculations(){
-	let totalEventBudgetDisplay = Number(document.getElementById("00N0b00000CbKPj").value);
+	let totalEventBudgetDisplay = Number(parseFloat(document.getElementById("00N0b00000CbKPj").value.replace(/,/g, ''));
 	let totalEventBudget = totalEventBudgetDisplay / 100;
 	percentDTPIDFundsTotal = Math.floor(requestedAmount / totalEventBudget);
 	document.getElementById("percentDTPIDFundsTotalDisplay").textContent = percentDTPIDFundsTotal;
-	numberWithCommas(totalEventBudgetDisplay.value);
+	totalEventBudgetDisplay.value.toLocaleString();
 }
 
 function numberWithCommas(x) {
