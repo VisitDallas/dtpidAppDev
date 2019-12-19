aba0e7a9d5a6cbe21b57cf3585cd85018188a9ad
Nick Askam <asknickfilms@gmail.com>
2019-12-19 11:14:51 -0600

    removing second variable

--- public/assets/js/events.js
+++ public/assets/js/events.js
@@ -2,10 +2,9 @@ let totalEventBudget = 0;
 let percentDTPIDTotal = 0;
 
 function doTotalBudgetCalculations(){
-	let totalEventBudgetDisplay.value = parseFloat(document.getElementById("00N0b00000CbKPj").value.replace(/,/g, ''));
-	let totalEventBudgetDisplay2 = totalEventBudgetDisplay;
+	let totalEventBudgetDisplay.value = document.getElementById("00N0b00000CbKPj").value;
 	totalEventBudget = Number(totalEventBudgetDisplay.value) / 100;
 	percentDTPIDFundsTotal = Math.floor(requestedAmount / totalEventBudget);
 	document.getElementById("percentDTPIDFundsTotalDisplay").textContent = percentDTPIDFundsTotal;
-	totalEventBudgetDisplay.value = numberWithCommas(totalEventBudgetDisplay2);
+	numberWithCommas(totalEventBudgetDisplay.value);
 }
