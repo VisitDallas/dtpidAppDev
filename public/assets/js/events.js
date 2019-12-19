03aa5c1df808d6f38f04829ac7d01b466157e2db
Nick Askam <asknickfilms@gmail.com>
2019-12-19 11:18:59 -0600

    removing .value

--- public/assets/js/events.js
+++ public/assets/js/events.js
@@ -2,7 +2,7 @@ let totalEventBudget = 0;
 let percentDTPIDTotal = 0;
 
 function doTotalBudgetCalculations(){
-	let totalEventBudgetDisplay.value = document.getElementById("00N0b00000CbKPj").value;
+	let totalEventBudgetDisplay.value = document.getElementById("00N0b00000CbKPj");
 	totalEventBudget = Number(totalEventBudgetDisplay.value) / 100;
 	percentDTPIDFundsTotal = Math.floor(requestedAmount / totalEventBudget);
 	document.getElementById("percentDTPIDFundsTotalDisplay").textContent = percentDTPIDFundsTotal;
