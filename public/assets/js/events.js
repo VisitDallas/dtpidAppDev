eeafa7273c3986c4ce1e0ef91068dcc78af0e670
Nick Askam <asknickfilms@gmail.com>
2019-12-19 11:40:54 -0600

    changing number with commas string

--- public/assets/js/events.js
+++ public/assets/js/events.js
@@ -1,7 +1,11 @@
 function doTotalBudgetCalculations(){
 	let totalEventBudgetDisplay = document.getElementById("00N0b00000CbKPj").value;
-	let totalEventBudget = Number(totalEventBudgetDisplay) / 100;
+	let totalEventBudget = Number(totalEventBudgetDisplay)	 / 100;
 	let percentDTPIDFundsTotal = Math.floor(requestedAmount / totalEventBudget);
 	document.getElementById("percentDTPIDFundsTotalDisplay").textContent = percentDTPIDFundsTotal;
 	numberWithCommas(totalEventBudgetDisplay.value);
+}
+
+function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
